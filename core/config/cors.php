e3d2b53d18b6c761f6b5a4eec2970493f2ec97a2
doerkonik <hasan.konik@doer.com.bd>
2026-04-05 15:42:29 +0600

    Add 'search/*' to CORS paths

--- core/config/cors.php
+++ core/config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'ipn/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'ipn/*', 'search/*'],
 
     'allowed_methods' => ['*'],
 
